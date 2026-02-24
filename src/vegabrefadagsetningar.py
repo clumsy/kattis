5fde88b690e233f984bd338749d0686f9dddf23d
Alexander Zhipa <alex.zhipa@proton.me>
2026-02-23 23:01:58 -0500

    https://open.kattis.com/problems/vegabrefadagsetningar

---
+++ src/vegabrefadagsetningar.py
@@ -0,0 +1,17 @@
+m = {
+    "JAN": "01",
+    "FEB": "02",
+    "MAR": "03",
+    "APR": "04",
+    "MAY": "05",
+    "JUN": "06",
+    "JUL": "07",
+    "AUG": "08",
+    "SEP": "09",
+    "OCT": "10",
+    "NOV": "11",
+    "DEC": "12",
+}
+d, mi, me, y = input().split()
+res = f"20{y}-{m[me[1:]]}-{d}"
+print(res)
